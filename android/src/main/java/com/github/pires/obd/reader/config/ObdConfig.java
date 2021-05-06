be95c137abf19a8afa854a11a8331cb8cc67d322
Narendra Bagade <narendra.bagade@copart.com>
2021-05-06 15:34:59 +0530

    update obdconfig - remove command from queue

--- android/src/main/java/com/github/pires/obd/reader/config/ObdConfig.java
+++ android/src/main/java/com/github/pires/obd/reader/config/ObdConfig.java
@@ -50,9 +50,9 @@ public final class ObdConfig {
 //         cmds.add(new VinCommand());
 
         // Engine
-        cmds.add(new LoadCommand());
+//         cmds.add(new LoadCommand());
         cmds.add(new RPMCommand());
-        cmds.add(new RuntimeCommand());
+//         cmds.add(new RuntimeCommand());
 //         cmds.add(new MassAirFlowCommand());
         cmds.add(new ThrottlePositionCommand());
 
